e7b1612e382ac7722916a3b5d2b3dd94775bf473
hameedfiaz <rmahameed005@gmail.com>
2015-05-09 12:09:03 +0530

    sidebar js changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $.slidebars({
     siteClose: false
   });
-  $.slidebars.toggle('left');
+  consumerToggle();
 });
 
 function displayOnMap(position){
@@ -18,3 +18,39 @@ function displayOnMap(position){
   handler.map.centerOn(marker);
   handler.map.serviceObject.setZoom(12);
 };
+
+function consumerToggle(){
+  $.slidebars.toggle("left");
+  $('#right_menu_button').fadeToggle();
+}
+
+function searchForService(service){
+  $("#services_modal").modal("hide");
+  toggleRightMenu();
+}
+
+function toggleRightMenu() {
+  $.slidebars.toggle("right");
+  $('#right_menu_button').fadeToggle();
+}
+
+function toggleFullScreen() {
+  // if (!document.fullscreenElement &&    // alternative standard method
+  //     !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+  //   if (document.documentElement.requestFullscreen) {
+  //     document.documentElement.requestFullscreen();
+  //   } else if (document.documentElement.mozRequestFullScreen) {
+  //     document.documentElement.mozRequestFullScreen();
+  //   } else if (document.documentElement.webkitRequestFullscreen) {
+  //     document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+  //   }
+  // } else {
+  //   if (document.cancelFullScreen) {
+  //     document.cancelFullScreen();
+  //   } else if (document.mozCancelFullScreen) {
+  //     document.mozCancelFullScreen();
+  //   } else if (document.webkitCancelFullScreen) {
+  //     document.webkitCancelFullScreen();
+  //   }
+  // }
+}
